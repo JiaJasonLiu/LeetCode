d732b384ce3c302b2eef76b4c2c2352673ebdf41
JiaJasonLiu <jialiu505@gmail.com>
2025-01-25 21:04:58 +0000

    some training

---
+++ Algorithms/PalindromeNumber.java
@@ -0,0 +1,11 @@
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int input = 121;
+        boolean result = isPalindrome(input);
+            System.out.println(result);
+    }
+}
---
+++ InterviewQuestions/Python/88_merge_sorted_array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        if n == 0:
+            return
+        if m == 0:
+            for i in range(n):
+                nums1[i] = nums2[i]
+    
+
+        for i in range(m, m+n):
+            nums1[i] = nums2[i - m]
+        nums1.sort()
